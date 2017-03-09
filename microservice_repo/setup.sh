6d1eaa06ac2882d8636ca84b996eae785af65f7b
Brian Balsamo <balsamo@uchicago.edu>
2017-03-09 11:41:22 -0600

    Switching the git clones to https for ease of use.

--- microservice_repo/setup.sh
+++ microservice_repo/setup.sh
@@ -3,12 +3,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/.env
 for x in $DIR/configs/*; do envsubst < $x > `echo $x | rev | cut -c 10- | rev`.py; done
-git clone git@github.com:uchicago-library/ldr_ingress.git && \
+git clone https://github.com/uchicago-library/ldr_ingress.git && \
     cp ./configs/ingress_conf.py ldr_ingress/config.py
-git clone git@github.com:uchicago-library/idnest.git && \
+git clone https://github.com/uchicago-library/idnest.git && \
     cp ./configs/idnest_conf.py idnest/config.py
-git clone git@github.com:uchicago-library/ldr_materialsuite_endpoint.git && \
+git clone https://github.com/uchicago-library/ldr_materialsuite_endpoint.git && \
     cp ./configs/materialsuite_endpoint_conf.py ldr_materialsuite_endpoint/config.py
-git clone git@github.com:uchicago-library/PREMISer.git
+git clone https://github.com/uchicago-library/PREMISer.git
     cp ./configs/premiser_conf.py PREMISer/config.py
-git clone git@github.com:uchicago-library/ldracquisitions_interface.git 
+git clone https://github.com/uchicago-library/ldracquisitions_interface.git 
